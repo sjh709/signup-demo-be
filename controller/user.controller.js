411e2d2120b9074115da32f4766168ee0002391a
Jihee <sjh709@naver.com>
2024-05-30 19:02:14 +0900

    로그인

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,7 +8,6 @@ userController.createUser = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     const user = await User.findOne({ email });
-    console.log('user', user);
     if (user) {
       throw new Error('이미 가입이 된 유저입니다.');
     }
@@ -17,6 +16,23 @@ userController.createUser = async (req, res) => {
     const newUser = new User({ email, name, password: hash });
     await newUser.save();
     res.status(200).json({ status: 'success' });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, '-createdAt -updatedAt -__v');
+    if (user) {
+      const isMath = bcrypt.compareSync(password, user.password);
+      if (isMath) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: 'success', user, token });
+      }
+    }
+    throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.');
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
