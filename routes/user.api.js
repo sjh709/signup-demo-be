eb8b2217e3f1777d7e4337022db003d1dd64700c
Jihee <sjh709@naver.com>
2024-06-03 05:09:01 +0900

    토큰으로 유저정보 찾기

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userController = require('../controller/user.controller');
+const authController = require('../controller/auth.controller');
 const router = express.Router();
 
 // 회원가입
 router.post('/', userController.createUser);
 // 로그인
 router.post('/login', userController.loginWithEmail);
+// 토큰을 통해 유저 id => 그 id로 유저 객체 찾아서 보내주기
+router.get('/me', authController.authenticate, userController.getUser);
 
 module.exports = router;
