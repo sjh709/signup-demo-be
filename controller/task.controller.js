ee982d0bc9551473e91b8dc5cd773e8d51d86cf1
Jihee <sjh709@naver.com>
2024-06-03 06:50:23 +0900

    작성자 추가하기

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -5,7 +5,8 @@ const taskController = {};
 taskController.createTask = async (req, res) => {
   try {
     const { task, isComplete } = req.body;
-    const newTask = new Task({ task, isComplete });
+    const { userId } = req;
+    const newTask = new Task({ task, isComplete, author: userId });
     await newTask.save();
     res.status(200).json({ status: 'success', data: newTask });
   } catch (err) {
@@ -15,7 +16,8 @@ taskController.createTask = async (req, res) => {
 
 taskController.getTask = async (req, res) => {
   try {
-    const taskList = await Task.find({}).select('-__v');
+    const taskList = await Task.find({}).select('-__v').populate('author');
+
     res.status(200).json({ status: 'success', data: taskList });
   } catch (err) {
     res.status(400).json({ status: 'fail', error: err });
